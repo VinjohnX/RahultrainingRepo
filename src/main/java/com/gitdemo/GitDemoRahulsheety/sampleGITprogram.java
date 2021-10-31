a7d47c82dff469730edced60c52713cdc3ff513f
VinjohnX <vinjohn2803@gmail.com>
2021-11-01 00:06:56 +0530

    First comiet from Rahul shetty training

---
+++ src/main/java/com/gitdemo/GitDemoRahulsheety/sampleGITprogram.java
@@ -0,0 +1,12 @@
+package com.gitdemo.GitDemoRahulsheety;
+
+public class sampleGITprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sample program to pratice GIT//GITHUB");
+
+	}
+
+}
