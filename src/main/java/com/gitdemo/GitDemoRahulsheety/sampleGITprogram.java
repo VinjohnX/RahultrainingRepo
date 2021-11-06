dad3228a421fcdbda88c3509972af601080d8360
VinjohnX <vinjohn2803@gmail.com>
2021-11-06 21:34:12 +0530

    The Third commit from Y guy

--- src/main/java/com/gitdemo/GitDemoRahulsheety/sampleGITprogram.java
+++ src/main/java/com/gitdemo/GitDemoRahulsheety/sampleGITprogram.java
@@ -13,5 +13,9 @@ public class sampleGITprogram {
 		System.out.println("Sample Program to pratice GIT/GITUB 6");
 
 	}
+	public void stunts()
+	{
+		System.out.println("The programs are not working..OOOH");
+	}
 	
 }
